eb2bcd1eb6f9d5638344f608cae3ac2af416a4b3
Drew Zucker <drew.zucker@parivedasolutions.com>
2017-07-19 12:19:02 -0400

    decisions added to anchor page. Keeps a running tally of how many times each option was chosen.

--- src/pages/anchors/anchors.ts
+++ src/pages/anchors/anchors.ts
@@ -6,9 +6,69 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'anchors.html'
 })
 export class AnchorsPage {
+	options: any[];
+	indexOne: number;
+	indexTwo: number;
 
   constructor(public navCtrl: NavController) {
-
+  	this.indexOne = 0;
+  	this.indexTwo = 1;
+  	this.populateOptions();
   }
 
+  decisionClicked(decision){
+  	decision.count++;
+  	if(!(this.indexTwo == 7)){
+  		this.indexTwo++;
+  	} else{
+  		this.indexOne++;
+  		this.indexTwo = this.indexOne + 1;
+  	}
+  	alert(decision.count);
+  }
+
+  populateOptions(){
+  	this.options = [
+  		{
+	  		title: "Technical/Functional",
+	  		description: "",
+	  		count: 0
+	  	},
+	  	{
+	  		title: "General managerial",
+	  		description: "",
+	  		count: 0
+	  	},
+	  	{
+	  		title: "Autonomy/Independence",
+	  		description: "",
+	  		count: 0
+	  	},
+	  	{
+	  		title: "Security/Stability",
+	  		description: "",
+	  		count: 0
+	  	},
+	  	{
+	  		title: "Entrepreneurial creativity",
+	  		description: "",
+	  		count: 0
+	  	},
+	  	{
+	  		title: "Service/Dedication to a cause",
+	  		description: "",
+	  		count: 0
+	  	},
+	  	{
+	  		title: "Pure challenge",
+	  		description: "",
+	  		count: 0
+	  	},
+	  	{
+	  		title: "Lifestyle",
+	  		description: "",
+	  		count: 0
+	  	},
+  	];
+  }
 }
