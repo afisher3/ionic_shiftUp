12045159b32dac09b2fbbcdbd5a00159a4b3ef51
Drew Zucker <drew.zucker@parivedasolutions.com>
2017-07-27 11:02:48 -0400

    add logo, a 'take test' button on homepage, and a little of a color scheme

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AnchorsPage } from '../anchors/anchors';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,11 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+  	
+  }
 
+  takeQuiz(){
+  	this.navCtrl.push(AnchorsPage);
   }
 
 }
